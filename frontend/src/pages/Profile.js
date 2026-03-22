ae044db31c450fde750cb052150a8c6a281dda8e
Reymond <r6544083@gmail.com>
2026-03-22 09:19:56 +0800

    Delete frontend/src/pages directory

--- frontend/src/pages/Profile.js
+++
@@ -1,9 +0,0 @@
-export default function Profile(){
- return(
-  <div className="container">
-   <h2>User Profile</h2>
-   <p>Name: Sample User</p>
-   <p>Email: user@email.com</p>
-  </div>
- );
-   }
