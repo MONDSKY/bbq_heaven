28c4aedc499bc91d85efe6c2fe52634eca81741b
Reymond <r6544083@gmail.com>
2026-03-22 09:40:29 +0800

    Update App.js

--- bbq_heaven/frontend/src/App.js
+++ bbq_heaven/frontend/src/App.js
@@ -3,6 +3,8 @@ import Login from "./pages/Login";
 import Customer from "./pages/Customer";
 import Admin from "./pages/Admin";
 import Profile from "./pages/Profile";
+import Receipt from "./pages/Receipt";
+import OrderTracking from "./pages/OrderTracking";
 
 export default function App(){
  return(
@@ -12,6 +14,8 @@ export default function App(){
  <Route path="/customer" element={<Customer/>}/>
  <Route path="/admin" element={<Admin/>}/>
 <Route path="/profile" element={<Private><Profile/></Private>}/>
+<Route path="/receipt" element={<Receipt/>}/>
+<Route path="/tracking" element={<OrderTracking/>}/>
  </Routes>
  </BrowserRouter>
  );
