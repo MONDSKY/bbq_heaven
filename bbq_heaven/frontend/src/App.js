ead7b97695c0478e62d4f690e8cc9675f2cf5989
Reymond <r6544083@gmail.com>
2026-03-22 09:23:15 +0800

    Update App.js

--- bbq_heaven/frontend/src/App.js
+++ bbq_heaven/frontend/src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Login from "./pages/Login";
 import Customer from "./pages/Customer";
 import Admin from "./pages/Admin";
+import Profile from "./pages/Profile";
+
 export default function App(){
  return(
  <BrowserRouter>
@@ -9,6 +11,7 @@ export default function App(){
  <Route path="/" element={<Login/>}/>
  <Route path="/customer" element={<Customer/>}/>
  <Route path="/admin" element={<Admin/>}/>
+<Route path="/profile" element={<Private><Profile/></Private>}/>
  </Routes>
  </BrowserRouter>
  );
