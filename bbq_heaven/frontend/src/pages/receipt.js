38273a23daf872aee1f890db4a037bcaa527d5c2
Reymond <r6544083@gmail.com>
2026-03-22 09:37:55 +0800

    Create Receipt.js

---
+++ bbq_heaven/frontend/src/pages/receipt.js
@@ -0,0 +1,12 @@
+
+export default function Receipt(){
+ return(
+  <div className="container">
+   <h2>Payment Successful</h2>
+   <p>Your order is now being prepared.</p>
+   <a href="/customer">
+    <button>Back to Menu</button>
+   </a>
+  </div>
+ );
+}
